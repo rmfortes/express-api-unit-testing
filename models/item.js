57fbc3b4188ac68b7624d7483c6cf49e3d55a363
REBECCA_FORTES <RMFORTES@UMICH.EDU>
2022-03-21 13:47:26 -0400

    seeds item data and builds associations

--- models/item.js
+++ models/item.js
@@ -3,23 +3,24 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class Item extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  }
-  Item.init({
+  const Item = sequelize.define('Item', {
     title: DataTypes.STRING,
     link: DataTypes.STRING,
-    userId: DataTypes.INTEGER
-  }, {
-    sequelize,
-    modelName: 'Item',
-  });
+    userId: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'User',
+        key: 'id',
+        as: 'userId',
+      }
+    }
+  }, {});
+  Item.associate = function (models) {
+    // associations can be defined here
+    Item.belongsTo(models.User, {
+      foreignKey: 'userId',
+      onDelete: 'CASCADE'
+    })
+  };
   return Item;
 };
